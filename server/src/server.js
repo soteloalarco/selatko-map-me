a9d9314a8c20e3d47c5065f2045a7c5b812eb1b2
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-28 14:52:07 -0600

    trying initial deployment in heroku

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 import mongoose from 'mongoose';
+import path from 'path';
+import express from 'express';
 import app from './express';
 
 require('dotenv').config();
@@ -13,6 +15,16 @@ mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/selatko-
 mongoose.connection.on('error', () => {
   throw new Error(`Unable to connect to database: ${process.env.DATABASE_URL || 'mongodb://localhost:27017/selatko-map-me'}`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT || 1337, (err) => {
   if (err) {
