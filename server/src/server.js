a29e9c65b843e9dce2f533881c7b81efa1d6301d
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-28 01:47:49 -0600

    retrying build in travis

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ import app from './express';
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/selatko-map-me', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
