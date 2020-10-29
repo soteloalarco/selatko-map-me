018ae12bba21890bfc53edff97eed400afdc961e
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-28 22:21:28 -0600

    retrying deployment

--- server/src/server.js
+++ server/src/server.js
@@ -18,11 +18,11 @@ mongoose.connection.on('error', () => {
   throw new Error(`Unable to connect to database: ${process.env.DATABASE_URL || 'mongodb://localhost:27017/selatko-map-me'}`);
 });
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 1337, (err) => {
