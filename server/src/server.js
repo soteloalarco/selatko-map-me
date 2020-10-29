8c75dda4bab7141f01d10b3429a5d49e417977ab
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-28 21:59:17 -0600

    retrying deployment

--- server/src/server.js
+++ server/src/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '../../client/build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 1337, (err) => {
