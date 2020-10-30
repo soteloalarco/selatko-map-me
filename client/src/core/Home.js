bb7b1af413241b0e8ebe514ea8cdd4fd73aea601
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-10-30 02:27:45 -0600

    map box sdk working and theme changes

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,42 +1,61 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import wherethewatertasteslikewine from '../assets/images/wolf-stories.png';
+import ReactMapGL from 'react-map-gl';
+import Link from '@material-ui/core/Link';
+
+require('dotenv').config();
 
 const useStyles = makeStyles((theme) => ({
   card: {
-    maxWidth: 600,
+    maxWidth: 800,
     margin: 'auto',
-    marginTop: theme.spacing(5),
+    marginTop: theme.spacing(4),
+    backgroundColor: '#EEDA9A',
   },
   title: {
     padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
     color: theme.palette.openTitle,
   },
   media: {
-    minHeight: 400,
+    minHeight: 500,
   },
 }));
 
 export default function Home() {
   const classes = useStyles();
+
+  const [viewport, setViewport] = useState({
+    latitude: 23.50,
+    longitude: -89.87,
+    zoom: 3.9,
+  });
+
   return (
     <Card className={classes.card}>
-      <Typography variant="h6" className={classes.title}>
-        Home Page
+      <Typography className={classes.title}>
+        <i> &quot;All the stories and songs and myths and legends start somewhere... with a seed. As they&apos;re told and re-told and passed around, they grow and change to become the stories we know&quot; </i>
+        (WTWTLW).
       </Typography>
-      <CardMedia
-        className={classes.media}
-        image={wherethewatertasteslikewine}
-        title="Unicorn Bicycle"
+      <ReactMapGL
+        {...viewport}
+        width="100vw"
+        height="60vh"
+        mapStyle="mapbox://styles/soteloalarco/ckgfu81286ioh19pgv8isi8w0"
+        ReactMapGL
+        mapboxApiAccessToken={process.env.MAP_BOX_TOKEN}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
       />
       <CardContent>
         <Typography variant="body2" component="p">
-          Image obtained from videogame Where The Water Tastes Like Wine.
+          Created by Rolando Sotelo,
+          <Link href="https://soteloalarco.github.io/"> Contact Me.</Link>
         </Typography>
       </CardContent>
     </Card>
