943bcf426808652d2eb20178eec9a296da587a45
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2020-11-04 23:23:25 -0600

    POST method for api/entries

---
+++ client/src/map/api-mapEntry.js
@@ -0,0 +1,18 @@
+/* eslint-disable consistent-return */
+const create = async (mapEntry) => {
+  try {
+    const response = await fetch('/api/entries', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(mapEntry),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default create;
